a2c65f192e3677a9d4bcb7378bbd1a48a17b8c80
arpit <arpitmaurya1506@gmail.com>
2025-08-22 22:25:21 +0530

    server

--- dtu-times-backend/server.js
+++ dtu-times-backend/server.js
@@ -13,6 +13,12 @@ app.use(cors({
 	credentials: true
 }));
 
+// Log every request URL
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+  next();
+});
+
 // Routes
 
 app.use('/api/auth', require('./routes/auth'));
